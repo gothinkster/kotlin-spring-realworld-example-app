1e784e75f68a50904ec49a1b1ef2c38a7725fd94
Alexandre Grison <a.grison@gmail.com>
2017-05-05 23:37:31 +0200

    Validation for Login and Register

--- src/main/kotlin/io/realworld/model/inout/Login.kt
+++ src/main/kotlin/io/realworld/model/inout/Login.kt
@@ -2,12 +2,14 @@ package io.realworld.model.inout
 
 import com.fasterxml.jackson.annotation.JsonRootName
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 @JsonRootName("user")
 class Login {
     @NotNull(message = "can't be missing")
     @Size(min = 1, message = "can't be empty")
+    @Pattern(regexp="^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$", message="must be a valid email")
     var email: String? = ""
 
     @NotNull(message = "can't be missing")
--- src/main/kotlin/io/realworld/model/inout/Register.kt
+++ src/main/kotlin/io/realworld/model/inout/Register.kt
@@ -2,16 +2,19 @@ package io.realworld.model.inout
 
 import com.fasterxml.jackson.annotation.JsonRootName
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 @JsonRootName("user")
 class Register {
     @NotNull(message = "can't be missing")
     @Size(min = 1, message = "can't be empty")
+    @Pattern(regexp="^\\w+$", message = "must be alphanumeric")
     var username: String? = ""
 
     @NotNull(message = "can't be missing")
     @Size(min = 1, message = "can't be empty")
+    @Pattern(regexp="^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$", message="must be a valid email")
     var email: String? = ""
 
     @NotNull(message = "can't be missing")
